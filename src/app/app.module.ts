564c73ef1d7650e30b11ebffe17312cecdc06176
javidabishov <javidabishov@azercell.com>
2022-08-17 00:34:55 +0400

    add new username-input component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import {FormsModule} from "@angular/forms";
+import { UsernameInputComponent } from './username-input/username-input.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WarningAlertComponent,
-    SuccessAlertComponent
+    SuccessAlertComponent,
+    UsernameInputComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule
   ],
   providers: [],
---
+++ src/app/username-input/username-input.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-username-input',
+  templateUrl: './username-input.component.html',
+  styleUrls: ['./username-input.component.css']
+})
+export class UsernameInputComponent implements OnInit {
+  username: string = '';
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  resetUsername() {
+    this.username = '';
+  }
+
+}
